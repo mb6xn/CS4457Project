0b9e5eda466e440e12a33a218a363493f582ca7c
Saad Khan <sk4gw@virginia.edu>
2018-12-02 13:15:31 -0500

    Tones added to Q3

--- client.py
+++ client.py
@@ -1,25 +1,46 @@
 import socket
 import numpy as np
-import sounddevice as sd
 from scipy.io.wavfile import write
-
+import scikits.audiolab
 #from rtlsdr import *
 
 
 TCP_IP = '172.25.79.75' # Saad's IP
 TCP_PORT = 4000
 BUFFER_SIZE = 1024
-MESSAGE = ("this is the pepsi and saad is very sexy")
-bin_data = bin(int.from_bytes(MESSAGE.encode(), 'big'))
-msg = np.array(list(bin_data[2:]), dtype = 'uint8')
-print (msg)
-sd.play(msg, 44100)
-#broadcast message on FM88.7
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.sendall(msg)
-msg = s.recv(BUFFER_SIZE)
-s.close()
+def gen_tone(amplitude, tone_duration, frequency):
+    x = np.arange(44100)
+    tone = amplitude * np.sin(2 * np.pi * frequency/44100 * x)
 
-print ("sent data:", msg)
+    return tone
+
+def modulateFSK(array, tone1, tone2):
+    for i in array:
+        if i == 1:
+            scikits.audiolab.play(tone1)
+        else:
+            scikits.audiolab.play(tone2)
+
+def main():
+    tone1 = gen_tone(100,1, 100)
+    tone2 = gen_tone(100,1, 900)
+
+    MESSAGE = ("this is the pepsi and saad is very sexy")
+    bin_data = bin(int.from_bytes(MESSAGE.encode(), 'big'))
+    msg = [int(i) for i in bin_data[2:]]
+    print(msg)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+
+    while True:
+        modulateFSK(msg, tone1, tone2)
+        s.sendall(msg)
+        msg = s.recv(BUFFER_SIZE)
+
+    s.close()
+    print(msg)
+
+if __name__ == '__main__':
+    main()
