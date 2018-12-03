65f9619171b9edd27f6e2bfa2e6bde8ae665145a
Saad Khan <sk4gw@virginia.edu>
2018-12-03 17:25:16 -0500

    server now plays sound

--- client.py
+++ client.py
@@ -1,30 +1,15 @@
 import socket
 import numpy as np
 from scipy.io.wavfile import write
-import sounddevice as sd
 
 TCP_IP = '127.0.0.1' # 172.25.98.219 Saad's IP
 TCP_PORT = 4000
 BUFFER_SIZE = 1024
 
-def gen_tone(amplitude, tone_duration, frequency):
-    x = np.arange(44100)
-    tone = amplitude * np.sin(2 * np.pi * frequency/44100 * x)
-    return tone
-
-tone1 = gen_tone(100, 1, 100)
-tone2 = gen_tone(100, 1, 900)
-
 MESSAGE = ("Wagwan m8")
 bin_data = bin(int.from_bytes(MESSAGE.encode(), 'big'))
 msg = np.array(list(bin_data[2:]), dtype = 'uint8')
 print(msg)
-
-for i in np.nditer(msg):
-    if i == 1:
-        sd.play(tone1, blocking = True)
-    else:
-        sd.play(tone2, blocking = True)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
--- server.py
+++ server.py
@@ -3,11 +3,19 @@ import numpy as np
 import pylab as plt
 from rtlsdr import RtlSdr
 import scipy.signal as signal
-
+import sounddevice as sd
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 4000
 BUFFER_SIZE = 4096
+
+def gen_tone(amplitude, tone_duration, frequency):
+    x = np.arange(44100)
+    tone = amplitude * np.sin(2 * np.pi * frequency/44100 * x)
+    return tone
+
+tone1 = gen_tone(100, 1, 100)
+tone2 = gen_tone(100, 1, 900)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -18,6 +26,11 @@ print ('Connection by', addr)
 
 while 1:
     msg = conn.recv(BUFFER_SIZE)
+    for i in list(msg):
+        if i == 1:
+            sd.play(tone1, blocking = True)
+        else:
+            sd.play(tone2, blocking = True)
     if not msg: break
     print ("Received Data:", msg)
     conn.sendall(msg)  # echo
