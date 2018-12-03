71c22ea80a92b551e6fab6bc42c16719430fddff
Saad Khan <sk4gw@virginia.edu>
2018-12-03 15:28:01 -0500

    Generated tones

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.io.wavfile import write
 import sounddevice as sd
 
-TCP_IP = '127.0.0.1' # Saad's IP
+TCP_IP = '127.0.0.1' # 172.25.98.219 Saad's IP
 TCP_PORT = 4000
 BUFFER_SIZE = 1024
 
@@ -15,16 +15,16 @@ def gen_tone(amplitude, tone_duration, frequency):
 tone1 = gen_tone(100, 1, 100)
 tone2 = gen_tone(100, 1, 900)
 
-MESSAGE = ("What is going on boys and girls")
+MESSAGE = ("Wagwan m8")
 bin_data = bin(int.from_bytes(MESSAGE.encode(), 'big'))
 msg = np.array(list(bin_data[2:]), dtype = 'uint8')
 print(msg)
 
 for i in np.nditer(msg):
     if i == 1:
-        sd.play(tone1, 44100)
+        sd.play(tone1, blocking = True)
     else:
-        sd.play(tone2, 44100)
+        sd.play(tone2, blocking = True)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
