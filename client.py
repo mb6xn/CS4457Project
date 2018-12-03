2c4b4b09d17e2a3f7f14460453318b02de35121e
Saad Khan <sk4gw@virginia.edu>
2018-12-03 03:04:09 -0500

    updated version of q3

--- client.py
+++ client.py
@@ -1,46 +1,35 @@
 import socket
 import numpy as np
 from scipy.io.wavfile import write
-import scikits.audiolab
-#from rtlsdr import *
+import sounddevice as sd
 
-
-TCP_IP = '172.25.79.75' # Saad's IP
+TCP_IP = '127.0.0.1' # Saad's IP
 TCP_PORT = 4000
 BUFFER_SIZE = 1024
 
 def gen_tone(amplitude, tone_duration, frequency):
     x = np.arange(44100)
     tone = amplitude * np.sin(2 * np.pi * frequency/44100 * x)
-
     return tone
 
-def modulateFSK(array, tone1, tone2):
-    for i in array:
-        if i == 1:
-            scikits.audiolab.play(tone1)
-        else:
-            scikits.audiolab.play(tone2)
+tone1 = gen_tone(100, 1, 100)
+tone2 = gen_tone(100, 1, 900)
 
-def main():
-    tone1 = gen_tone(100,1, 100)
-    tone2 = gen_tone(100,1, 900)
+MESSAGE = ("What is going on boys and girls")
+bin_data = bin(int.from_bytes(MESSAGE.encode(), 'big'))
+msg = np.array(list(bin_data[2:]), dtype = 'uint8')
+print(msg)
 
-    MESSAGE = ("this is the pepsi and saad is very sexy")
-    bin_data = bin(int.from_bytes(MESSAGE.encode(), 'big'))
-    msg = [int(i) for i in bin_data[2:]]
-    print(msg)
+for i in np.nditer(msg):
+    if i == 1:
+        sd.play(tone1, 44100)
+    else:
+        sd.play(tone2, 44100)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.sendall(msg)
+msg = s.recv(BUFFER_SIZE)
+s.close()
 
-    while True:
-        modulateFSK(msg, tone1, tone2)
-        s.sendall(msg)
-        msg = s.recv(BUFFER_SIZE)
-
-    s.close()
-    print(msg)
-
-if __name__ == '__main__':
-    main()
+print("Data sent: ", msg)
--- server.py
+++ server.py
@@ -1,26 +1,25 @@
 import socket
 import numpy as np
-import sounddevice as sd
 import pylab as plt
 from rtlsdr import RtlSdr
 import scipy.signal as signal
 
 
-TCP_IP = ''
+TCP_IP = '127.0.0.1'
 TCP_PORT = 4000
-BUFFER_SIZE = 20
+BUFFER_SIZE = 4096
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(5)
 
 conn, addr = s.accept()
-print ('Connection address:', addr)
+print ('Connection by', addr)
 
 while 1:
     msg = conn.recv(BUFFER_SIZE)
     if not msg: break
-    print ("received data:", msg)
+    print ("Received Data:", msg)
     conn.sendall(msg)  # echo
 
 conn.close()
