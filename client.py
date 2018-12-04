d5783884cb90468b9f1b520cdde63dec535a5dda
Matt B <mb6xn@virginia.edu>
2018-12-04 02:43:52 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -6,10 +6,18 @@ TCP_IP = '172.25.79.180' # 172.25.98.219 Saad's IP
 TCP_PORT = 4000
 BUFFER_SIZE = 1024
 time.sleep(2)
-MESSAGE = ("a")
-bin_data = bin(int.from_bytes(MESSAGE.encode(), 'big'))
-msg = np.array(list(bin_data[2:]), dtype = 'uint8')
-print(msg)
+MESSAGE = ("a")  #let's read in num=len(message) bytes because ASCII values go up to 127, then right shift by 1.
+file = open("key.txt", "rb") #works for larger strings
+num=len(MESSAGE)
+key= bin(int.from_bytes(file.read(num), 'big')>>1)[2:]
+#print("Key:       "+key)
+
+bin_data = (bin(int.from_bytes(MESSAGE.encode(), 'big')))[2:]
+#print("Data:      "+bin_data)
+
+encrypt = bin(int(bin_data,2) ^ int(key,2))[2:].zfill(len(key))  #same for decryption
+#print("Encrypted: "+encrypt)
+msg = np.array(list(encrypt), dtype = 'uint8')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
